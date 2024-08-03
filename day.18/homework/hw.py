85b95349b1d3a6d0630d00cdadddf2ca4aeed96a
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-08-03 10:02:49 -0700

    1

---
+++ day.18/homework/hw.py
@@ -0,0 +1,26 @@
+for i in str("gio","dato","lizi"):
+    print(i)
+
+for i in range(51,40,3):
+    print(i)
+
+for i in range(2,51,4):
+    print(i)
+
+for i in str("luka", "nika","data", "lasha"):
+    print(i)
+
+for i in range(402,932,821):
+    print(i) 
+
+for i in range(1,3,6):
+    print(i)   
+
+for i in int(5,302,10):
+    print(i)  
+
+for i in range(1,62,9):
+    print(i)   
+
+for i in range(1,62,9):
+    print(i)       
