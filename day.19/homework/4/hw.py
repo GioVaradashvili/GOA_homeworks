562225ef3368cdf7089f907988ef937fc643a05b
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-08-06 01:10:47 -0700

    1

---
+++ day.19/homework/4/hw.py
@@ -0,0 +1,14 @@
+
+my_name = "პირველი სახელი"
+
+
+while True:
+
+    user_name = input("გთხოვთ, შეიყვანეთ თქვენი სახელი: ")
+
+   
+    if user_name == my_name:
+        print("სწორი სახელი!")
+        break
+    else:
+        print("არასწორი სახელი, სცადეთ ხელახლა.")
