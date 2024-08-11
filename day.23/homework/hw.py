d01b18051a03a39f482bc6953fb09d83e7252c8a
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-08-11 14:17:46 -0700

    1

---
+++ day.23/homework/hw.py
@@ -0,0 +1,147 @@
+#  pirveli
+
+# def opperations(a, b):
+#     num = a + b
+#     number = a - b
+#     integer = a * b
+#     list = a / b if b != 0 else ''
+#     return num, number, integer, list
+
+# print(opperations(15, 9))
+
+# meore
+
+# def number(a, b):
+#     while a < 100:
+#         a += b
+#     return a
+
+# print(number(50, 20))
+
+# mesame
+
+# def num(number):
+#     return number % 2 == 0
+
+# print(num(6))  
+# print(num(3))  
+
+# meotxe
+
+# def max_number(lst):
+#     if not lst:
+#         return None
+#     return max(lst)
+
+# print(max_number([7, 10, 2, 51, 5]))
+
+# mexute
+
+# def jam_of_numbers(lst):
+#     return sum(lst)
+
+# print(jam_of_numbers([4, 9, 7, 66, 2]))
+
+# meeqse
+
+# def rendom_list(*args):
+#     return args[::-1]
+
+# print(rendom_list('orange', 5.0, 'alucha', 3.0))
+
+# meshvide
+
+# def animals(strings):
+#     if not strings:
+#         return None, None
+#     longest = max(strings, key=len)
+#     shortest = min(strings, key=len)
+#     return longest, shortest
+
+# print(animals(['mela', 'maimuni', 'hipopotami', 'ku', 'zebra', 'mgeli', 'drakoni', 'cxeni', 'viri']))
+
+# merve
+
+# def string(s):
+#     return s.swapcase()
+
+# print(string("Giorgi Varadashvili"))
+
+# mecxre
+
+# def tanxmovani(s):
+#     vowels = "aeiouAEIOU"
+#     return sum(1 for char in s if char.isalpha() and char not in vowels)
+
+# print(tanxmovani("gamarjoba khaketo"))
+
+# meate
+
+# def kenti_luwi(number):
+#     return number % 2 == 0
+
+# print(kenti_luwi(5))  #kentia
+# print(k(8))  #luwia
+
+# metertmete
+
+# def luwi(numbers):
+#     return sum(numbers[i] for i in range(0, len(numbers), 2))
+
+# print(luwi([8, 21, 33, 24, 6, 9]))
+
+# metormete
+
+# def luwia_tu_kenti(number):
+#     return 'luwia' if number % 2 == 0 else 'kentia'
+
+# print(luwia_tu_kenti(4))  
+# print(luwia_tu_kenti(9))  
+
+# print(luwia_tu_kenti(5))
+# print(luwia_tu_kenti(10))
+
+# mecamete
+
+# def is_prime(number):
+#     if number <= 1:
+#         return False
+#     for i in range(2, int(number ** 0.5) + 1):
+#         if number % i == 0:
+#             return False
+#     return True
+
+
+# print(is_prime(11))  
+# print(is_prime(15))  
+
+# metotxmete
+
+# def capitalize_names(names):
+#     return [name.capitalize() for name in names]
+
+# print(capitalize_names(["john", "doe", "jane"])) 
+
+# metxutmete
+
+# def numbers_lists(numbers):
+#     result = []
+#     for number in numbers:
+#         if number % 2 == 0:
+#             result.append(number // 2)
+#         else:
+#             result.append(number * 2)
+#     return result
+
+# print(numbers_lists([1, 2, 3, 4])) 
+
+
+# meteqsvmete
+
+# def upper(s):
+#     return s[::-1].upper()
+
+# print(upper("example"))  
+
+
+# anarchenebi ver gavige 17 18 19 da 20
