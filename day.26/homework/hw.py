c93925958257f30b9dd8a9ec338545075325c716
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-08-12 14:40:22 -0700

    1

---
+++ day.26/homework/hw.py
@@ -0,0 +1,333 @@
+# 1
+
+# for i in range(1, 6):
+#     print(i)
+
+ # 2
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# 3
+
+# for char in "hello":
+#     print(char)
+
+# 4
+
+# s = "hello"
+# index = 0
+# while index < len(s):
+#     print(s[index])
+#     index += 1
+
+# 5
+
+# for i in range(1, 4):
+#     print(i)
+
+# 6
+
+# i = 1
+# while i <= 3:
+#     print(i)
+#     i += 1
+
+# 7
+
+# for i in range(3):
+#     print("GOA BEST!")
+
+# 8
+
+# num = 0
+# while num < 3:
+#     print("GOA BEST!")
+#     num += 1
+
+# 9
+
+# number_list = [1, 2, 3]
+# for i in number_list:
+#     print(i)
+
+# 10
+
+# number_list = [1, 2, 3]
+# num = 0
+# while num < len(number_list):
+#     print(number_list[num])
+#     num += 1
+
+# 11
+
+# for i in [1, 2, 3]:
+#     print("hi")
+
+# 12
+
+# number = [1, 2, 3]
+# num = 0
+# while num < len(number):
+#     print("hi")
+#     num += 1
+
+# 13
+
+# for i in range(1, 4):
+#     print(i)
+
+# 14
+
+# number = 1
+# while number <= 3:
+#     print(number)
+#     number += 1
+
+# 15
+
+# for i in range(3, 0, -1):
+#     print(i)
+
+# 16
+
+# i = 3
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# 17
+
+# for string in "abcd":
+#     print(string)
+
+# 18
+
+# string = "ABCD"
+# number = 0
+# while number < len(string):
+#     print(string[number])
+#     string += 1
+
+# 19
+
+# for i in range(4):
+#     print("looping")
+
+# 20
+
+# num = 0
+# while num < 4:
+#     print("looping")
+#     num += 1
+
+# 21
+
+# for i in (1, 2, 3):
+#     print(i)
+
+# 22
+
+# number_lists = (1, 2, 3)
+# num = 0
+# while num < len(number_lists):
+#     print(number_lists[num])
+#     num += 1
+
+# 23
+
+# for i in range(5, 0, -1):
+#     print(i)
+  
+# 24
+
+# num = 5
+# while num >= 1:
+#     print(num)
+#     num -= 1
+
+# 25
+
+# xili = ["apple", "banana", "cherry"]
+# for asorti in xili:
+#     print(asorti)
+
+# 26
+
+# fruits = ["apple", "banana", "cherry"]
+# index = 0
+# while index < len(fruits):
+#     print(fruits[index])
+#     index += 1
+
+#  27
+
+# for i in range(3):
+#     print(i)
+
+# 28
+
+# num = 0
+# while num < 3:
+#     print(num)
+#     num += 1
+
+# 29
+
+# numbers = [1, 2, 3, 4]
+# for i in numbers:
+#     print("loop")
+  
+# 30
+
+# numbers = [1, 2, 3, 4]
+# i = 0
+# while i < len(numbers):
+#     print("loop")
+#     i += 1
+
+# 31
+
+# for i in "abc":
+#     print(i)
+
+# 32
+
+# anbani = "abc"
+# i = 0
+# while i < len(anbani):
+#     print(anbani[i])
+#     i += 1
+
+# 33
+
+# letters = ["x", "y", "z"]
+# for i in range(2):
+#     print(letters[i])
+
+# 34
+
+# letters = ["x", "y", "z"]
+# num = 0
+# while num < 2:
+#     print(letters[num])
+#     num += 1
+
+# 35
+
+# for i in range(2):
+#     print("Hello World!")
+
+# 36
+
+# num = 0
+# while num < 2:
+#     print("Hello World")
+#     num += 1
+
+# 37
+
+# num = {1, 2, 3}
+# for i in num:
+#     print(i)
+
+# 38
+
+# number = {1, 2, 3}
+# number_list = list(number) 
+# index = 0
+# while index < len(number_list):
+#     print(number_list[index])
+#     index += 1
+
+
+# 39
+
+# dictator = {"a": 1, "b": 2}
+# for num1, num2 in dictator.items():
+#     print(f"{num1}: {num2}")
+
+# 40
+
+# dictator = {"a": 1, "b": 2}
+# num2 = list(dictator.keys()) 
+# i = 0
+# while i < len(num2):
+#     num1 = num2[i]
+#     print(f"{num1}: {dictator[num2]}")
+#     i += 1
+
+# 41
+
+# for i in range(10, 31, 10):
+#     print(i)
+
+
+# 42
+
+# num = 10
+# while num <= 30:
+#     print(num)
+#     num += 10
+
+# 43
+
+# for i in range(5):
+#     print(i)
+# print("Done")
+
+# 44
+
+# num = 0
+# while num < 5:
+#     print(num)
+#     num += 1
+# print("Done")
+
+
+# 45
+
+# l1st = [[1, 2], [3, 4]]
+# for sublist in l1st:
+#     for num in sublist:
+#         print(num)
+
+# 46
+
+# l1st = [[1, 2], [3, 4]]
+# index = 0
+# while index < len(l1st):
+#     i = l1st[index]
+#     i = 0
+#     while i < len(i):
+#         print([i])
+#     i += 1
+#     index += 1
+
+# 47
+
+# for i in range(1, 6):
+#     print(i)
+
+
+# 48
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# 49
+
+# for i in "loop":
+#     print(i)
+
+# 50
+
+# s = "loop"
+# i = 0
+# while i < len(s):
+#     print(s[i])
+#     i += 1
